535b2b5fe964f9b30cc758ae900a2f5d85a8d5da
Richy Osano <richardosano97@gmail.com>
2024-10-17 23:42:30 +0300

    Update automationBuilder.test.tsx

--- src/__tests__/automationBuilder.test.tsx
+++ src/__tests__/automationBuilder.test.tsx
@@ -1,5 +1,4 @@
-import React, { act } from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import AutomationBuilder from '@/app/components/AutomationBuilder';
 import { useAutomationBuilder } from '@/app/hooks/useAutomationBuilder';
